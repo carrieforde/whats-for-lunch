c2f0ce7723039a82f1d7cf3308568046995f9b13
dforde <darren.forde@gmail.com>
2021-04-03 15:57:10 -0700

    Add axios based query

--- src/hooks/usePlacesApi/usePlacesApi.ts
+++ src/hooks/usePlacesApi/usePlacesApi.ts
@@ -1,25 +1,35 @@
 import { useState } from "react";
 import { UsePlacesApi } from "./usePlacesApi.interface";
 
+import axios from "axios";
+
 const usePlacesApi = (entityName: string): UsePlacesApi => {
-  const [place, setPlace] = useState<any>(undefined);
+  const [place, setPlace] = useState<UsePlacesApi>();
+
+  const PLACES_PATH = `/maps/api/place/findplacefromtext/json`;
 
   const findPlace = async () => {
     try {
-      const res = await fetch(
-        `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(
-          entityName
-        )}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${
-          process.env.REACT_APP_GOOGLE_API_KEY
-        }`,
-        {
-          mode: "no-cors",
-        }
-      );
-      const data = await res.json();
+      const res = await axios({
+        method: "get",
+        url: PLACES_PATH,
+        baseURL: process.env.REACT_APP_PLACES_BASE_URL,
+        params: {
+          input: entityName,
+          inputtype: "textquery",
+          fields:
+            "business_status,formatted_address,geometry,icon,name,place_id,plus_code,types,opening_hours,price_level,rating,user_ratings_total",
+          key: process.env.REACT_APP_GOOGLE_API_KEY,
+          type: "restaurant",
+          location_bias: "circle:5000@37.579200,-122.319430",
+        },
+      });
+
+      const data = res.data;
+      console.log(data);
       setPlace(data);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 
